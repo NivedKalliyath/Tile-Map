2b5c7842febaeb5ad0fd12b1526f6b76d5500c4a
nived kalliyath <nivedkalliyath@gmail.com>
2024-11-10 00:50:38 +0530

    Final with lights

--- DALL·E 2024-11-07 21.17.08 - A vibrant retro-styled 'greater than' symbol (_) in pixel art with neon pink, purple, and blue colors, in the same style as a pixelated '2'. The image.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="DALL·E 2024-11-07 21.17.08 - A vibrant retro-styled 'greater than' symbol (_) in pixel art with neon pink, purple, and blue colors, in the same style as a pixelated '2'. The image" tilewidth="10" tileheight="18" tilecount="1" columns="1">
- <image source="../../DALL·E 2024-11-07 21.17.08 - A vibrant retro-styled 'greater than' symbol (_) in pixel art with neon pink, purple, and blue colors, in the same style as a pixelated '2'. The image.jpg" width="10" height="18"/>
-</tileset>
---
+++ abstract-background-design-hd-light-alphabet-red-color_851755-123013.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="abstract-background-design-hd-light-alphabet-red-color_851755-123013" tilewidth="3" tileheight="2" tilecount="1" columns="1">
+ <image source="../../../Downloads/abstract-background-design-hd-light-alphabet-red-color_851755-123013.png" width="4" height="2"/>
+</tileset>
